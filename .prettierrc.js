1b696c3a3423fbca06dd6ee32cbe69306e10fda1
MikeSquall <michael.arbib@ownera.io>
2022-06-08 11:23:37 +0300

    Project configuration

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: 'typescript',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
